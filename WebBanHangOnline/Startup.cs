61ef7f780f4bbb7c2cc0eddd41a4b9c324455b93
phamlengocthao <phamlengocthao18122004@gmail.com>
2024-11-28 14:39:41 +0700

    backup

---
+++ WebBanHangOnline/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Services.Description;
+
+[assembly: OwinStartupAttribute(typeof(WebBanHangOnline.Startup))]
+namespace WebBanHangOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            ConfigureMvc();
+        }
+        public void ConfigureMvc()
+        {
+            // Đăng ký các route cho ứng dụng
+            System.Web.Routing.RouteTable.Routes.MapMvcAttributeRoutes();  // Nếu bạn sử dụng attribute routing
+            RouteConfig.RegisterRoutes(System.Web.Routing.RouteTable.Routes);  // Cấu hình route mặc định
+        }
+    }
+}
